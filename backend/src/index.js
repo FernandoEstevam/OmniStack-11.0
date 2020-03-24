d4739ec7c690744e2e0b36cc77aeaeac9fd9a7e1
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-24 20:27:47 -0300

    Incluindo o module route no index

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,36 +7,6 @@ const app = express();
 
 app.use(express.json());
 
-// Rota Raiz
-app.get('/', (resquest, response) => {
-  return response.json({
-    evento: "Semana OmniStack 11.0",
-    data: new Date()
-  });
-});
-
-// Routes Params
-app.get('/users/:id', (request, response) => {
-  const params = request.params;
-
-  console.log(params);
-
-  return  response.json({
-    evento: "Semana OmniStack 11.0",
-    data: new Date()
-  });
-});
-
-// Routes Body
-app.post('/users', (request, response) => {
-  const body = request.body;
-
-  console.log(body);
-
-  return  response.json({
-    evento: "Semana OmniStack 11.0",
-    data: new Date()
-  });
-});
+app.use(routes);
 
 app.listen(3333);
