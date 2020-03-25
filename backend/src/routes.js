ff1d743e414cb7d6ba61a7d37940395b615feb62
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-25 14:44:31 -0300

    Adicionado script para deletar incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,12 +4,19 @@ const express = require('express');
 
 // Controllers
 const OngController = require('../src/controllers/OngController');
+const IncidentController = require('../src/controllers/IncidentController');
 
 const connection = require('./database/connection');
 
 const routes = express.Router();
 
+// ONGS
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+// Incidents
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
