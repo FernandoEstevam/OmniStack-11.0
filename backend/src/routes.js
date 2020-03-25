7e84bc10df9d3bdd269a36c388e1f3cb46641956
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-25 15:36:15 -0300

    Criando rotas session login, criando routa profile

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,10 +5,15 @@ const express = require('express');
 // Controllers
 const OngController = require('../src/controllers/OngController');
 const IncidentController = require('../src/controllers/IncidentController');
+const ProfileController = require('../src/controllers/ProfileController');
+const SessionController = require('../src/controllers/SessionController');
 
 const connection = require('./database/connection');
 
 const routes = express.Router();
+
+// Sessions(login e logout)
+routes.post('/sessions', SessionController.create);
 
 // ONGS
 routes.get('/ongs', OngController.index);
@@ -18,5 +23,7 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
