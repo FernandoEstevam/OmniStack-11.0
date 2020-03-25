aaf34f4bedd231bb4a2d0ffce491ccc5bbe91bb0
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-25 12:33:24 -0300

    Criando uma pasta controller para separar os scripts e restruturar o projeto

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+  // Lista todas a ONGS
+  async index(request, response) {
+      const ongs = await connection('ongs').select('*');
+    
+      return response.json(ongs);
+  },
+
+  // Cria ONG
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+  
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    })
+    
+    return response.json({ id });
+  }
+}
