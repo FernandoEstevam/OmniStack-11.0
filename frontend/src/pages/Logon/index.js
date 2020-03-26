3ac4694b0beddd1032727745eaf9bfd3fadea238
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-26 15:38:06 -0300

    Finalizando o componente Logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
 
+// import css
 import './styles.css';
+// import image
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
   return(
-    <div>
-      
-    </div>
+  <div className="logon-container">
+    <section className="form">
+      <img src={logoImg} alt="Be The Hero"/>
+
+      <form >
+        <h1>Faça seu logo</h1>
+
+        <input placeholder="Sua ID" />
+        <button type="submit" className="button">Entar</button>
+
+        <Link to="/register">
+          <FiLogIn size={16} color='#E02041' />
+          Não tem cadastro
+        </Link>
+      </form>
+    </section>
+    
+    <img src={heroesImg} alt="Heroes" />
+  </div>
   )
 }
