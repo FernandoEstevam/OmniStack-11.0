63f89592092639054815c82851b6e3cac7dd91ad
Fernando Estevam <fernandoestevam23@gmail.com>
2020-03-27 20:04:41 -0300

    Finalização do mobile

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -14,16 +14,33 @@ import styles from './styles';
 
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigation = useNavigation();
 
-  function navigateToDetail() {
-    navigation.navigate('Detail');
+  function navigateToDetail(incident) {
+    navigation.navigate('Detail', { incident });
   }
   
   async function loadIncidents() {
-    const response = await api.get('incidents');
+    if(loading) {
+      return;
+    }
+
+    if(total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get(`incidents?pages=${page}`);
     
-    setIncidents(response.data);
+    setIncidents([...incidents, ...response.data]);
+    setTotal(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -35,7 +52,7 @@ export default function Incidents() {
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total de <Text style={styles.headerTextBold}>0 casos.</Text>
+          Total de <Text style={styles.headerTextBold}>{total} casos.</Text>
         </Text>
       </View>
 
@@ -47,20 +64,28 @@ export default function Incidents() {
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
           <Text style={styles.incidentProperty}>ONG:</Text>
           <Text style={styles.incidentValue}>{incident.name}</Text>
-
+          <Text>{incident.id}</Text>
           <Text style={styles.incidentProperty}>CASO:</Text>
           <Text style={styles.incidentValue}>{incident.title}</Text>
 
           <Text style={styles.incidentProperty}>VALOR:</Text>
-          <Text style={styles.incidentValue}>{Intl incident.value}</Text> 
+          <Text style={styles.incidentValue}>{
+            Intl
+            .NumberFormat('pt-BR',{
+              style: 'currency', 
+              currency: 'BRL'
+            })
+            .format(incident.value)}</Text> 
 
           <TouchableOpacity 
             style={styles.detailButton} 
-            onPress={navigateToDetail}
+            onPress={() => navigateToDetail(incident)}
           >
             <Text style={styles.detailButtonText}>Ver mais detalhes</Text>
             <Feather name="arrow-right" size={16} color="#E02041"/>
